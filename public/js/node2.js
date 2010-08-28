9d599a7b9d21e9c45bccea13fcbfb6dcd478ead9
Jan Lelis <mail@janlelis.de>
2010-08-28 04:58:15 +0200

    node

--- public/js/node2.js
+++ public/js/node2.js
@@ -6,5 +6,17 @@ if(typeof(console) == 'undefined') console = {log: function () {}};
 // draggable
 $(".draggable").draggable();
 
+// socket test code ...
+var socket = new io.Socket(location.hostname, {
+    transports: ['websocket', 'xhr-polling', 'xhr-multipart', 'server-events', 'htmlfile', 'flashsocket']
+});
+socket.on('connect', function() {
+    console.log('socket connected');
+});
+socket.on('message', function(msg) {
+    console.log('message incoming: ' + msg);
+});
+socket.connect();
+
 // close
 });
