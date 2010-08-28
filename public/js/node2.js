55595dce37eab329223344d1f058e1c6e280e1cf
dodo <dodo.the.last@googlemail.com>
2010-08-28 08:29:21 +0200

    add some fency animation

--- public/js/node2.js
+++ public/js/node2.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
+$(".holder").animate({width:'-=15px'}, 500);
 
 // firefox workaround
 if(typeof(console) == 'undefined') console = {log: function () {}};
 
 // draggable
 $(".draggable").draggable();
+
+$(".holder").hover(
+    function () {$(this).animate({width:'+=15px'}, 100);},//handleIn
+    function () { $(this).animate({width:'-=15px'}, 100);} //handleOut
+);
 
 // socket test code ...
 var socket = new io.Socket(location.hostname, {
