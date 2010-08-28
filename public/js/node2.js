df6b8ad73620b9d2fb46285dd402b61514079ffe
Jan Lelis <mail@janlelis.de>
2010-08-28 04:19:20 +0200

    draggable etc

--- public/js/node2.js
+++ public/js/node2.js
@@ -3,5 +3,8 @@ $(document).ready(function(){
 // firefox workaround
 if(typeof(console) == 'undefined') console = {log: function () {}};
 
+// draggable
+$(".draggable").draggable();
+
 // close
 });
