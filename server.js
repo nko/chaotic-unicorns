58f171b2ffd39213661488a829b818315ffcdfee
Thammi <thammi@chaossource.net>
2010-08-28 03:36:06 +0200

    Adding db to server.js and fixing db

--- server.js
+++ server.js
@@ -5,42 +5,45 @@
 require.paths.unshift(__dirname + '/lib');
 
 var express = require('express'),
-    connect = require('connect');
+    connect = require('connect'),
+    db = require('./db');
 
-var app = module.exports = express.createServer();
+db.connect(function(dbc) {
+    var app = module.exports = express.createServer();
 
-// Configuration
+    // Configuration
 
-app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.use(connect.bodyDecoder());
-    app.use(connect.methodOverride());
-    app.use(connect.compiler({ src: __dirname + '/public', enable: ['less'] }));
-    app.use(app.router);
-    app.use(connect.staticProvider(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-    app.use(connect.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-   app.use(connect.errorHandler()); 
-});
-
-// Routes
-
-app.get('/', function(req, res){
-    res.render('index.haml', {
-        locals: {
-            title: 'Node²'
-        }
+    app.configure(function(){
+        app.set('views', __dirname + '/views');
+        app.use(connect.bodyDecoder());
+        app.use(connect.methodOverride());
+        app.use(connect.compiler({ src: __dirname + '/public', enable: ['less'] }));
+        app.use(app.router);
+        app.use(connect.staticProvider(__dirname + '/public'));
     });
+
+    app.configure('development', function(){
+        app.use(connect.errorHandler({ dumpExceptions: true, showStack: true })); 
+    });
+
+    app.configure('production', function(){
+       app.use(connect.errorHandler()); 
+    });
+
+    // Routes
+
+    app.get('/', function(req, res){
+        res.render('index.haml', {
+            locals: {
+                title: 'Node²'
+            }
+        });
+    });
+
+    // Only listen on $ node app.js
+
+    if (!module.parent) {
+        app.listen(parseInt(process.env.PORT) || 3000);
+        console.log("server listening on port %d ...", app.address().port)
+    }
 });
-
-// Only listen on $ node app.js
-
-if (!module.parent) {
-    app.listen(parseInt(process.env.PORT) || 3000);
-    console.log("server listening on port %d ...", app.address().port)
-}
