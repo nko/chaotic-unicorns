0392ed067b26247402b08fdc942f98da4fa92b32
dodo <dodo.the.last@googlemail.com>
2010-08-29 15:26:17 +0200

    rewritten springs physics engine

--- public/js/joint.js
+++ public/js/joint.js
@@ -1,171 +1,161 @@
 
-// spring vars
+var springsPhysics = function () {
 
-var spring_length = 0;
-var spring_strength = 5;
-var spring_mass = 2342;
-var spring_charge = 3;
+    // defaults
 
-// helper
+    var _default = {
+            length:   10,
+            strength: 50,
+            mass:     1342,
+            charge:   30,
+            friction: 0.9,
+        };
 
-var min = function (s1,s2) {if(s2>s1) return s1; else return s2};
-var max = function (s1,s2) {if(s1>s2) return s1; else return s2};
+    // helper
 
-var vmag = function (vec) {return Math.sqrt(vec.x*vec.x+vec.y*vec.y);};
-var vmul = function (vec,a) {return {x: vec.x*a, y: vec.y*a}};
-var vnorm = function (vec) {if(vec.x == 0 && vec.y == 0) return vec; else return vmul(vec, 1 / vmag(vec));};
-var vadd = function (a,b) {return {x:a.x+b.x, y:a.y+b.y};};
-var vsub = function (a,b) {return {x:a.x-b.x, y:a.y-b.y};};
+    var _position_helper = function (offset,obj) {
+        var off = {x: obj.offset().left, y: obj.offset().top};
+        var mid = {x: obj.width() / 2,   y: obj.height() / 2};
+        var pos = {x:off.x - offset.left  + mid.x,
+                   y:off.y - offset.top   + mid.y};
+        return {offset:off, middle:mid, position:pos};
+    };
 
+    var min = function (s1,s2) {if(s2>s1) return s1; else return s2};
+    var max = function (s1,s2) {if(s1>s2) return s1; else return s2};
 
-var initSpring = function (_node) {$(_node).attr('speed',"0,0");};
+    var vmag = function (vec) {return Math.sqrt(vec.x*vec.x+vec.y*vec.y);};
+    var vmul = function (vec,a) {return {x: vec.x*a, y: vec.y*a}};
+    var vadd = function (a,b) {return {x:a.x+b.x, y:a.y+b.y};};
+    var vsub = function (a,b) {return {x:a.x-b.x, y:a.y-b.y};};
+    var vnorm = function (vec) {if(vec.x == 0 && vec.y == 0)
+        return vec; else return vmul(vec, 1 / vmag(vec));};
 
+    // library
 
-var position_helper = function (offset,obj) {
-    var off = obj.offset();
-    var mid = {top:  obj.height() / 2,
-              left: obj.width() / 2};
-    var pos = {x:off.left - offset.left + mid.left,
-               y:off.top - offset.top + mid.top};
-    return {offset:off, middle:mid, position:pos};
-};
+    var system = {};
+    var constructor = system.generate = function (params) {
+        if(typeof(params) == "undefined") params = _default;
+        // TODO more params check
+        var engine = build(params);
+        var realtime = function (ms) {
+            var dt = ms / 1000;
+            simulate(engine, dt);
+            animate(engine, dt);
+            draw(engine);
+        };
+        return {realtime:realtime};
+    };
 
-
-// canvas
-
-var updateCanvas = function () {
-    var obj = $("#canvas");
-    var canvas = obj[0];
-    canvas.height = obj.height();
-    canvas.width  = obj.width();
-    var offset = obj.offset();
-    var g = canvas.getContext("2d");
-    g.clearRect(0, 0, canvas.width, canvas.height);
-    g.beginPath();
-    var cache = {};
-    $(".node").each(function () {
-        var current = $(this);
-        var src = position_helper(offset, current);
-        $.each(current.attr("relation").split(","),function (_,relation) {
-            if(relation != "") {
-                var target = $("#"+relation);
-                if(target.length) {
-                    var hash = min(current[0].id,target[0].id) + "," + max(current[0].id,target[0].id);
-                    if(!(hash in cache)) {
-                        cache[hash] = 5;
-                        var trg = position_helper(offset, target);
-                        // finally drawing ...
-                        g.moveTo(src.position.x, src.position.y);
-                        g.lineTo(trg.position.x, trg.position.y);
-                  }
-              }
-            }
-        });
-    });
-    g.stroke();
-    g.closePath();
-};
-
-// spring physics engine
-
-var updateSprings = function (ms) {
-    var dt = ms/1000;
-    var obj = $("#canvas");
-    var offset = obj.offset();
-    // get all springs
-    var cache = {};
-    var accelerations = {};
-    $(".node").each(function () {
-        var current = $(this);
-        var src = position_helper(offset, current);
-        $.each(current.attr("relation").split(","),function (_,relation) {
-            if(relation != "") {
-                var target = $("#"+relation);
-                if(target.length) {
-                    var hash = min(current[0].id,target[0].id) + "," + max(current[0].id,target[0].id);
-                    if(!(hash in cache)) {
-                        var trg = position_helper(offset, target);
-                        // some accel calc
-                        var diff = vsub(src.position, trg.position);
-                        var dir = vnorm(diff);
-                        var difflen = spring_length - vmag(diff);
-                        var accel = vmul(dir, difflen * spring_strength);
-                        //save
-                        cache[hash] = 5;
-                        if(current[0].id in accelerations)
-                            accelerations[current[0].id].acceleration = vadd(accelerations[current[0].id].acceleration, accel);
-                        else accelerations[current[0].id] = {current: current,
-                                                       position: src.position,
-                                                       other: trg.position,
-                                                       acceleration: accel,
-                                                       mystirious:{x:src.middle.left, y:src.middle.top}
-                                                      };
-                        if(target[0].id in accelerations)
-                            accelerations[target[0].id].acceleration = vadd(accelerations[target[0].id].acceleration, accel);
-                        else accelerations[target[0].id] = {current: target,
-                                                       position: trg.position,
-                                                       other: src.position,
-                                                       acceleration: accel,
-                                                       mystirious:{x:trg.middle.left, y:trg.middle.top}
-                                                      };
-                        //calc some electic force (blitz!)
-                        var accel = {x:0,y:0};
-                        $(".node").each(function () {
-                            var another = $(this);
-                            if(current != another) {
-                                var atr = position_helper(offset, another);
-                                var diff = vsub(atr.position,src.position);
-                                var dir = vnorm(diff);
-                                var difflen = spring_length - diff.x*diff.x - diff.y*diff.y; //vmag(diff)^2
-                                if(Math.abs(difflen) > 1e2)
-                                  accel = vadd(accel,vmul(dir, (spring_charge*spring_charge)/-difflen));
-                            }
-                        });
-                        //save
-                        if(current[0].id in accelerations)
-                            accelerations[current[0].id].acceleration = vadd(accelerations[current[0].id].acceleration, accel);
-                        else accelerations[current[0].id] = {current: current,
-                                                       position: src.position,
-                                                       other: src.position,
-                                                       acceleration: accel,
-                                                       mystirious:{x:src.middle.left, y:src.middle.top}
-                                                      };
+    var build = system.build = function (params) {
+        if(typeof(params) == "undefined") params = _default;
+        var nodes = {}, joints = [];
+        var window = $("#nodes");
+        //get nodes
+        $(".node").each(function () {
+            var current = $(this);
+            var composition = _position_helper(window.offset(), current);
+            nodes[current[0].id] = cur_node = {
+                    object: current,
+                    position: composition.position,
+                    middle: composition.middle,
+                    offset: composition.offset,
+                    acceleration: {x:0, y:0},
+                    speed: {x:0, y:0},
+                    charge: params.charge,
+                    mass: params.mass,
+                    friction: params.friction,
+                    joints: [],
+                };
+            //get joints
+            $.each(current.attr("relation").split(","),function (_,relation) {
+                if(relation != "") {
+                    var target = $("#"+relation);
+                    if(target.length) {
+                        nodes[current[0].id].joints.push(joints.push({
+                                source: cur_node,
+                                target: target[0].id,
+                                length: params.length,
+                                strength: params.strength,
+                            }));
                     }
                 }
-            }
+            });
         });
-    });
-    // update
-    var canvas = obj[0];
-    canvas.height = obj.height();
-    canvas.width  = obj.width();
-    var g = canvas.getContext("2d");
-    g.clearRect(0, 0, canvas.width, canvas.height);
-    g.beginPath();
-    $.each(accelerations, function (_, node) {
-        var current = node.current;
-        if(!current.hasClass("root") && !current.hasClass("fixed")) {
-            // some speed calc
-            var speed = current.attr('speed').split(",");
-            speed = {x: parseFloat(speed[0]), y: parseFloat(speed[1])};
-            speed = vadd(speed, vmul(node.acceleration, dt/spring_mass ));
-            speed = vmul(speed, 0.9);
-            //console.log("speed", speed.x,speed.y,"|",dt);
-            var pos = vadd(node.position,speed);
-            // drawing ...
-            g.moveTo(node.other.x, node.other.y);
-            g.lineTo(pos.x, pos.y);
-            // applying new position
-            pos = vsub(pos, node.mystirious);
-            current.animate({left:pos.x, top:pos.y},dt);
-            // applying speed
-            current.attr('speed', speed.x+","+speed.y);
-        } else if(current.hasClass("fixed")) {
+        //fill relations with nodes
+        $.each(joints,function (_,joint) {joint.target = nodes[joint.target];});
+        return {nodes:nodes, joints:joints};
+    };
+
+    var simulate = system.run = function (engine, dt) {
+        //calc joint acceleration
+        $.each(engine.joints, function (_, joint) {
+            var diff = vsub(joint.target.position, joint.source.position);
+            var dir = vnorm(diff);
+            var difflen = vmag(diff) - joint.length;
+            var accel = vmul(dir, difflen * joint.strength);//<- calc stuff
+            joint.target.acceleration = vadd(joint.target.acceleration,accel);
+        });
+        //calc electric force acceleration (bl!tz)
+        $.each(engine.nodes, function (_, node) {
+            var accel = {x:0, y:0};
+            $.each(engine.nodes, function (_, other) {
+                if(other != node) {
+                    var diff = vsub(node.position,other.position);
+                    var dir = vnorm(diff);
+                    var difflen = diff.x*diff.x -
+                                  diff.y*diff.y; //vmag(diff)^2 //<- calc stuff
+                    if(Math.abs(difflen) > 1e2)
+                        accel = vadd(accel,vmul(dir,
+                            (other.charge*other.charge)/difflen));
+                }
+            });
+            node.acceleration = vadd(node.acceleration,accel);
+        });
+        //apply results
+        $.each(engine.nodes, function (_, node) {
+            // omitt all currently hovered nodes
+            if(!(node.object.hasClass("fixed"))) {
+                //calc speed
+                node.speed = vadd(node.speed, vmul(node.acceleration,dt/node.mass));
+                node.speed = vmul(node.speed, node.friction);// ^^^ calc stuff
+                //apply speed
+                node.position = vadd(node.position, node.speed);
+            }
+            //clean acceleration
+            node.acceleration = {x:0, y:0};
+        });
+        return engine;
+    };
+
+    var draw = system.draw = function (engine) {
+        var obj = $("#canvas");
+        var canvas = obj[0];
+        canvas.height = obj.height();
+        canvas.width  = obj.width();
+        var g = canvas.getContext("2d");
+        g.clearRect(0, 0, canvas.width, canvas.height);
+        g.beginPath();
+        $.each(engine.joints, function (_, joint) {
+            var src = vsub(joint.source.position,{x:0,y:joint.source.middle.y/2});//??
+            var trg = vsub(joint.target.position,{x:0,y:joint.target.middle.y/2});//??
              // drawing ...
-            g.moveTo(node.other.x, node.other.y);
-            g.lineTo(node.position.x, node.position.y);
-        }
-    });
-    g.stroke();
-    g.closePath();
-};
+            g.moveTo(src.x, src.y);
+            g.lineTo(trg.x, trg.y);
+        });
+        g.stroke();
+        g.closePath();
+        return engine;
+    };
+    
+    var animate = system.animate = function (engine, dt) {
+        $.each(engine.nodes, function (_, node) {
+            var pos = vsub(node.position, node.middle);
+            node.object.animate({left:pos.x, top:pos.y}, dt);
+        });
+        return engine;
+    };
+
+    return system;
+}();
+
